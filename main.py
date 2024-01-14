f932490c530d80bc1a1ce899735f985ec8b3eeed
Soulbolt <soulbolt@gmail.com>
2024-01-13 19:00:59 -0700

    implemented sqlalchemy code for database and schema creation to store books data.

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 app = Flask(__name__)
 
+# :ost to store book objects
 all_books = []
 
+# Database
+class Base(DeclarativeBase):
+    pass
+
+# configure the SQLite database, relative to the app instance folder
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
+
+# Extension
+db = SQLAlchemy(model_class=Base)
+
+# Initialize the app with the extension
+db.init_app(app)
+
+# Define Model
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    review: Mapped[float] = mapped_column(Float, nullable=False)
+# Create the table schema in the database, Requires application context
+# with app.app_context():
+#     db.create_all()
 
 @app.route('/')
 def home():
